d39c79fde893102aacc85a0f550c9a5707a769a2
Mike Lumetta <mjlumetta@gmail.com>
2016-03-13 20:16:11 -0500

    Add basic log in/out functionality

--- evolution_proto/config/routes.rb
+++ evolution_proto/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'static#home'
   get 'about', to: 'static#about'
   get 'register', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
